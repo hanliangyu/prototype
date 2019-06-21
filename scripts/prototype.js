025f0293ce158158b36e3f949c11dd00fb6c0a14
Liangyu Han <liangyu.han17@imperial.ac.uk>
2019-06-21 14:22:25 +0100

    Integrated all animations, quiz and text

--- scripts/prototype.js
+++ scripts/prototype.js
@@ -32,6 +32,15 @@ function showQ1SolutionButton() {
   }
 }
 
+function showQ1Solution() {
+  var x = document.getElementById('q1Solution');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
 
 /* Main Function */
 function main() {
