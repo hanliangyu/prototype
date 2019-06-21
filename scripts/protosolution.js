85d0a62de2d60bed6a0192956e6983335b822f08
Elizabeth <elizabeth.oon17@imperial.ac.uk>
2019-06-21 13:22:23 +0100

    Add quiz files

---
+++ scripts/protosolution.js
@@ -0,0 +1,8 @@
+function showQ1Solution() {
+  var x = document.getElementById('q1Solution');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
